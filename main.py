0c269f889289f5607a0ace48abd8a470ea45c2ea
Jefferson-Rafael-Oficial <contatojefferson25@proton.me>
2022-09-06 19:30:30 -0300

    esqueci de salvar antes de commitar

---
+++ main.py
@@ -0,0 +1,10 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+# Todos os códigos dos aplicativos no kivy começam com
+# class MyApp(App):
+#   pass
+class RegistradorTarefas(App):
+    def build(self):
+        return super().build()
